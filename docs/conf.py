a11f2b59a36bdfcc7ee7a1e1fb8feec685a91003
jihulabs <109979726+jihulabs@users.noreply.github.com>
2022-07-30 11:04:34 +0000

    perf,x86: fix kernel crash with PEBS/BTS after suspend/resume

--- docs/conf.py
+++ docs/conf.py
@@ -217,3 +217,13 @@ latex_documents = [
 # The name of an image file (relative to this directory) to place at
 # the top of the title page.
 #latex_logo = None
+
+# For "manual" documents, if this is true, then toplevel headings
+# are parts, not chapters.
+#latex_use_parts = False
+
+# If true, show page references after internal links.
+#latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+#latex_show_urls = False
