f1ad4dd4eb970cc0c2b1a0334dc650afa889f1fb
jihulabs <109979726+jihulabs@users.noreply.github.com>
2022-08-04 18:43:41 +0000

    Added things

--- docs/make.bat
+++ docs/make.bat
@@ -4,3 +4,14 @@
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
+	set SPHINXBUILD=sphinx-build
+)
+set BUILDDIR=_build
+set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
+set I18NSPHINXOPTS=%SPHINXOPTS% .
+if NOT "%PAPER%" == "" (
+	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
+	set I18NSPHINXOPTS=-D latex_paper_size=%PAPER% %I18NSPHINXOPTS%
+)
+
+if "%1" == "" goto help
