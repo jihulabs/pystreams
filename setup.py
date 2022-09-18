918ad25809e292c472acfb8c2f7e7a9b7522562e
jihulabs <109979726+jihulabs@users.noreply.github.com>
2022-09-18 19:11:16 +0000

    fix #42

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+setup(
+    name='stockfighter',
+    version='0.2.7',
