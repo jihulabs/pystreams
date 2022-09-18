f1f1e600d862ba4b3c9adfb127cfb4fc8cceaea0
jihulabs <109979726+jihulabs@users.noreply.github.com>
2022-09-18 19:23:45 +0000

    Add logo to readme

--- setup.py
+++ setup.py
@@ -18,3 +18,14 @@ with open('HISTORY.rst') as history_file:
 setup(
     name='stockfighter',
     version='0.2.7',
+    description="API wrapper for Stockfighter",
+    long_description=readme + '\n\n' + history,
+    author="Scott Triglia",
+    author_email='scott.triglia@gmail.com',
+    url='https://github.com/striglia/stockfighter',
+    packages=['stockfighter'],
+    package_dir={'stockfighter': 'stockfighter'},
+    include_package_data=True,
+    install_requires=[
+        'requests>=2.4.2',
+        'six',
