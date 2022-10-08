e7797f75c857a9661a48f9435e2acbd80067d5f9
jihulabs <109979726+jihulabs@users.noreply.github.com>
2022-10-08 07:39:15 +0000

    More structure

--- stockfighter/gm.py
+++ stockfighter/gm.py
@@ -1,2 +1,15 @@
 
 import os
+import requests
+from six.moves.urllib.parse import urljoin
+
+
+class GM(object):
+    base_url = 'https://www.stockfighter.io/gm/'
+
+    def __init__(self, api_key=None):
+        self.api_key = os.environ['API_KEY'] if api_key is None else api_key
+        self.headers = {'Cookie': 'api_key={api_key}'.format(api_key=self.api_key)}
+
+        # Data on which levels you have running.
+        self.level_data = self._load_data()
