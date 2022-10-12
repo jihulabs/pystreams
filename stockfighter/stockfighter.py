b175f3d2e54cfe7430605b2c16c31cb1fc966c38
jihulabs <109979726+jihulabs@users.noreply.github.com>
2022-10-12 22:37:13 +0000

    modify version

--- stockfighter/stockfighter.py
+++ stockfighter/stockfighter.py
@@ -7,3 +7,15 @@ import requests
 
 class Stockfighter(object):
     base_url = 'https://api.stockfighter.io/ob/api/'
+
+    def __init__(self, venue, account, api_key=None):
+        self.venue = venue
+        self.account = account
+
+        if api_key is not None:
+            self.api_key = api_key
+        else:
+            self.api_key = os.environ['API_KEY']
+
+        self.session = requests.Session()
+        self.session.headers.update({
