9d621938440fa931e2c16653ec9573e7e391f0ad
jihulabs <109979726+jihulabs@users.noreply.github.com>
2022-10-12 13:49:57 +0000

    docs: update for 2.0.0-beta.0

--- stockfighter/gm.py
+++ stockfighter/gm.py
@@ -21,3 +21,16 @@ class GM(object):
         resp = requests.get(url, headers=self.headers)
         # TOOD: Confirm/deny that this is a real API for the levels currenlty running...
         if resp.content:
+            return resp.json()
+        else:
+            return None
+
+    def start(self, level_name):
+        url = urljoin(self.base_url, 'levels/{level_name}'.format(level_name=level_name))
+        resp = requests.post(url, headers=self.headers)
+        return resp.json()
+
+    def restart(self, instance_id):
+        url = urljoin(self.base_url, 'instances/{instance_id}/restart'.format(instance_id=instance_id))
+        resp = requests.post(url, headers=self.headers)
+        return resp.json()
