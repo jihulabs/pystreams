3841bcaeb3bb75f9c6acfaba063833dcbf757603
jihulabs <109979726+jihulabs@users.noreply.github.com>
2023-06-04 15:17:02 +0000

    git-all automatic commit

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -107,3 +107,17 @@ def update_travis_deploy_password(encrypted_password):
 def main(args):
     public_key = fetch_public_key(args.repo)
     password = args.password or getpass('PyPI password: ')
+    update_travis_deploy_password(encrypt(public_key, password.encode()))
+    print("Wrote encrypted password to .travis.yml -- you're ready to deploy")
+
+
+if '__main__' == __name__:
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--repo', default=GITHUB_REPO,
+                        help='GitHub repo (default: %s)' % GITHUB_REPO)
+    parser.add_argument('--password',
+                        help='PyPI password (will prompt if not provided)')
+
+    args = parser.parse_args()
+    main(args)
